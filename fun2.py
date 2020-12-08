835c9f9869cf56b5a4951f6829acd86c64d7e882
Wojciech7738 <wwbutrym@gmail.com>
2020-12-08 20:48:00 +0100

    init

--- fun2.py
+++ fun2.py
@@ -1,208 +1,30 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
 from lbp import LocalBinaryPatterns
-from imutils import paths
 import sklearn.ensemble as se
-# import tensorflow as tf
-
-image_size = (300, 200)
-lbp_size = (8,2)
-
-def read_image(img_path):
-    img = cv2.imread(img_path)
-    img = cv2.resize(img, image_size)
-    return img
-
-def blurring(img):
-    # img = cv2.GaussianBlur(img, (5, 5), 0)
-    # img = cv2.bilateralFilter(img, 9, 75, 75)
-    img = cv2.blur(img, (3, 3))
-    return img
-
-
-def extract_single_image(img, RGB=False, plot=False):
-    local_bin_pat = LocalBinaryPatterns(*lbp_size)
-    LBP, hist = local_bin_pat.compute_LBP(img, RGB=RGB, plot=plot)
-
-    # Return the LBP image and the histogram
-    return LBP, hist
-
-
-def extract_multiple_images(path, RGB=False, plot=False, use_prev_data=False):
-    # Classes of images (0 if not given object, 1 otherwise)
-    classes = []
-    # Features - merged histograms
-    features = np.empty(0)
-
-    # Compute histogram for every file in directory
-    for imagePath in paths.list_images(path):
-        _, hist = extract_single_image(read_image(imagePath), RGB=RGB, plot=plot)
-
-        # Create a list of classes (0 if there is "notpir" in filename)
-        if not use_prev_data:
-            if "notpir" in imagePath:
-                classes.append(0)
-            else:
-                classes.append(1)
-
-        # The image features which will be returned
-        if len(features) == 0:
-            features = hist
-        else:
-            features = np.vstack((features, hist))
-
-    # Convert classes into numpy array
-    classes = np.asarray(classes)
-    return features, classes
-
-
-
-
-
-# Method which divides single image into windows
-def predict_single_image(classifier, imgPath, threshold, rows=11, columns=14, RGB=False, plot=False, plot_rect=False, proba=False):
-    # RGB - prediction for RGB-LBP method;      plot_rect - plot a rectangle on the current window's position to see
-    # if evetything works fine;     rows and columns - (...) on plot;   plot - show a plot;
-    # threshold - value of probability needed for each sample for being classified as "1"
-    # proba - print probability instead of full prediction in plot's title
-
-    image = read_image(imgPath)
-    # Blurring
-    # image = blurring(image)
-
-    responses = []
-    # Divide image into windows
-    # image_size is (300, 200), but the real one is REVERSED [cv2.resize function]
-    img_size = image_size[::-1]
-    percentage = 0.2 # percentage of cropped image relative to its original size
-    counter = 0.2 # is added in every loop iteration
-    i=0
-    while percentage < 1.09:
-        x = 0
-        y = 0
-        cr_img_size = (int(img_size[0]*percentage), int(img_size[1]*percentage))
-        while x < img_size[0] and y < img_size[1]:
-            i = i+1
-            cropped_image = image[x:cr_img_size[0]+x, y:cr_img_size[1]+y]
-            cropped_image = cv2.resize(cropped_image, image_size)
-
-            # Plot a rectangle
-            if plot:
-                if not plot_rect:
-                    plt.subplot(rows,columns,i)
-                    plt.imshow(cropped_image)
-                else:
-                    img_with_rectangle = cv2.rectangle(image, (y,x), (cr_img_size[1]+y,cr_img_size[0]+x), (255,int((percentage+0.4)*255),int((percentage+0.4)*255)),2)
-                    plt.imshow(img_with_rectangle)
-                    plt.show()
-
-            _, features = extract_single_image(cropped_image, RGB=RGB)
-
-            if not proba:
-                # if we want to see the probability instead of full prediction
-                responses.append(classifier.predict(features.reshape(1,-1)))
-                if plot:
-                    if responses[i-1] == 1: # alternatively: if responses[i-1][0][1] >= 0.55:
-                        plt.title("Pyramid")
-                    else:
-                        plt.title("Null")
-            else:
-                responses.append(classifier.predict_proba(features.reshape(1, -1)))
-                # round to two decimal places
-                responses[i - 1] = np.around(responses[i-1], 2)
-                if plot:
-                    plt.title(str(responses[i - 1]))
-
-            y = int(cr_img_size[1]/2)+y
-            if y >= img_size[1]:
-                x = int(cr_img_size[0]/2)+x
-                if x < img_size[0]:
-                    y = 0
-                # leave current y value for while stop condition otherwise
-        percentage = percentage + counter
-        if percentage >= 0.6:
-            counter = 0.4
-    if plot:
-        plt.show()
-
-    # Check every response
-    res = 0
-    for r in responses:
-        # modify some condition depending on selected type of response
-        if proba:
-            cond = r[0][1] >= threshold
-        else:
-            cond = r == 1
-        if cond:
-            res = 1
-    # if any is equal 1 - there is a pyramid on the image
-    return responses, res
-
-
-def predict_multiple_images(classifier, RGB=False, plot=False, proba=False):
-    features2, _ = extract_multiple_images('Images/Test', RGB=RGB, plot=plot, use_prev_data=True)
-    if not proba:
-        predictions = classifier.predict(features2)
-    else:
-        predictions = classifier.predict_proba(features2)
-
-    # plot images
-    i= 1
-    for imagePath in paths.list_images('Images/Test'):
-        plt.subplot(6,5,i)
-        plt.imshow(read_image(imagePath))
-
-        if not proba:
-            if predictions[i-1] == 0:
-                plt.title("Null")
-            else:
-                plt.title("Pyramid")
-        else:
-            plt.title(str(predictions[i-1]))
-        i = i+1
-    plt.show()
-    return predictions
-
-
-
-def advanced_predict_multiple_images(classifier, threshold, RGB=False, plot=False, plot_rect=False):
-    predictions = []
-    i = 1
-    for imagePath in paths.list_images('Images/Test'):
-        _, resp = predict_single_image(classifier, imagePath, threshold, RGB=RGB, plot=plot, plot_rect=plot_rect, proba=True)
-        predictions.append(resp)
-        # plot images
-        plt.subplot(6,5,i)
-        plt.imshow(read_image(imagePath))
-        if predictions[i-1] == 0:
-            plt.title("Null")
-        else:
-            plt.title("Pyramid")
-        i = i+1
-    plt.show()
-    return predictions
-
-
-
+import time
+# import pandas as pd, csv
 
 def Main():
+    lbp_size = (8, 2)
+    LBP = LocalBinaryPatterns(*lbp_size)
     # extract an matrix of histograms and vector of classes
-    features, classes = extract_multiple_images('Images/Train')
+    begin = time.time()
+    features, classes = LBP.extract_multiple_images('Images/Train')
+    end = time.time()
+    print(end-begin)
 
-    # Construct the strong classifier from image features (week classifier)
-    classifier = se.AdaBoostClassifier(n_estimators=features.shape[1], random_state=0)
-    classifier.fit(features, classes)
-
-    # Test trained model
-    # (without dividing images)
-    print(predict_multiple_images(classifier, proba=False))
-
-    # For comparison
-    _, response = predict_single_image(classifier, 'Images/Test/walshs-pyramid---south-of-cairn-36649_1280x731.jpg', 0.52, rows=10, columns=15, plot=True, proba=True)
-    print(response)
-    # advanced_predict_multiple_images(classifier, 0.52) # takes very long time
-    print("DONE")
+    # # Construct the strong classifier from image features (week classifier)
+    # classifier = se.AdaBoostClassifier(n_estimators=features.shape[1], random_state=0)
+    # classifier.fit(features, classes)
+    #
+    # # Test trained model
+    # # (without dividing images)
+    # print(LBP.predict_multiple_images(classifier, proba=False))
+    #
+    # # For comparison
+    # _, response = LBP.predict_single_image(classifier, 'Images/Test/walshs-pyramid---south-of-cairn-36649_1280x731.jpg', 0.52, rows=10, columns=15, plot=True, proba=True)
+    # print(response)
+    # # advanced_predict_multiple_images(classifier, 0.52) # takes very long time
+    # print("DONE")
 
 
 
--- lbp.py
+++ lbp.py
@@ -1,28 +1,82 @@
 import numpy as np
-from skimage import feature
 import cv2
 import matplotlib.pyplot as plt
+from imutils import paths
+
 
 class LocalBinaryPatterns:
     def __init__(self, nPoints, radius_of_scanning):
         self.nPoints = nPoints
         self.radius_of_scanning = radius_of_scanning
+        # gray image with size 312x416
+        # self.IMG = np.ascontiguousarray(self.__read_image__(imgPath), dtype=np.int)
+        # new image
+        # self.lbp_image = np.zeros(self.IMG.shape, dtype=np.int)
+        # window's side
+        self.square_side = 104
+        # positions of the neighbours (see how LBP works)
+        self.neighbour_positions = lambda row, col: [(row - 1, col - 1), (row - 1, col), (row - 1, col + 1),
+                                                     (row, col + 1), (row + 1, col + 1),
+                                                     (row + 1, col), (row + 1, col - 1), (row, col - 1)]
+        self.image_size = (416, 312)
 
-    # def local_binary_pattern(self, IMG, nPoints, radius_of_scanning):
+    def __read_image__(self, imgPath):
+        img = cv2.imread(imgPath)
+        return cv2.resize(img, (416, 312))
 
-    
-    def describe(self, image, RGB=False, eps=1e-7):
-        if RGB==True:
+    def local_binary_pattern(self, image):
+        image = np.ascontiguousarray(image, dtype=np.int)
+        self.lbp_image = np.zeros(image.shape, dtype=np.int)
+        # 1: divide image into grid
+        for r in range(3):
+            for c in range(4):  # columns
+                self.__lbp_computing__(image, r, c)
+        return self.lbp_image
+
+    def __lbp_computing__(self, image, r, c):
+        new_img = image[self.square_side * r:self.square_side * (r + 1),
+                  self.square_side * c:self.square_side * (c + 1)]
+        for i in range(self.square_side):
+            for j in range(self.square_side):
+                binary_values = []
+                current = new_img[i, j]
+                nei_position = self.neighbour_positions(i, j)
+                for k in range(len(nei_position)):
+                    # 2: compute LBP
+                    if self.square_side * r + nei_position[k][0] < 0 or \
+                            self.square_side * c + nei_position[k][1] < 0:
+                        binary_values.append(0)
+                    else:
+                        if nei_position[k][0] >= self.square_side or nei_position[k][1] >= self.square_side:
+                            if self.square_side * r + nei_position[k][0] >= image.shape[0] or \
+                                    self.square_side * c + nei_position[k][1] >= image.shape[1]:
+                                # if given point is beyond the image - write 0
+                                binary_values.append(0)
+                            else:
+                                self.__cond_check__(image, self.square_side * r + nei_position[k][0],
+                                                    self.square_side * c + nei_position[k][1], current, binary_values)
+                        else:
+                            self.__cond_check__(new_img, nei_position[k][0], nei_position[k][1], current, binary_values)
+                # convert into decimal
+                value = int(bin(int(''.join(map(str, binary_values)), 2)), 2)
+                self.lbp_image[self.square_side * r + i, self.square_side * c + j] = value
+
+    def __cond_check__(self, img, p1, p2, current, binary_values):
+        if img[p1][p2] < current:
+            binary_values.append(0)
+        else:
+            binary_values.append(1)
+
+    def describe(self, IMG, RGB=False, eps=1e-7):
+        if RGB == True:
             i = 3
         else:
-            i=1
+            i = 1
         for j in range(i):
-            if RGB==True:
-                IMG = image[:,:,j]
-            else:
-                IMG = image
-            lbp = feature.local_binary_pattern(IMG, self.nPoints, self.radius_of_scanning)
-            hist, _ = np.histogram(lbp.ravel(), bins=np.arange(0, 256, 1))
+            if RGB == True:
+                IMG = IMG[:, :, j]
+            lbp = self.local_binary_pattern(IMG)
+            hist, _ = np.histogram(lbp.ravel(), bins=np.arange(lbp.min(), lbp.max(), 1))
 
             # normalize the histogram
             hist = hist.astype("float")
@@ -34,8 +88,6 @@ class LocalBinaryPatterns:
         # return the histogram of Local Binary Patterns
         return Hist
 
-
-
     def compute_LBP(self, img, RGB=False, plot=False, transform_image=False):
         LBP = None
         if RGB == False:
@@ -43,16 +95,16 @@ class LocalBinaryPatterns:
             gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             if transform_image:
                 # "LBP" variable is not necessary for object detection. It only shows how LBP works
-                LBP = feature.local_binary_pattern(gray_img, self.nPoints, self.radius_of_scanning, method="uniform")
+                LBP = self.local_binary_pattern(gray_img)
             # only histogram is needed for object detection
             hist = self.describe(gray_img)
 
         else:
             method_name = "RGB-LBP"
             if transform_image:
-                R = feature.local_binary_pattern(img[:, :, 0], 8, 2, method="uniform")
-                G = feature.local_binary_pattern(img[:, :, 1], 8, 2, method="uniform")
-                B = feature.local_binary_pattern(img[:, :, 2], 8, 2, method="uniform")
+                R = self.local_binary_pattern(img[:, :, 0])
+                G = self.local_binary_pattern(img[:, :, 1])
+                B = self.local_binary_pattern(img[:, :, 2])
                 LBP = np.vstack((R, G))
                 LBP = np.vstack((LBP, B))
             # same
@@ -64,3 +116,172 @@ class LocalBinaryPatterns:
             plt.show()
 
         return LBP, hist
+
+    def blurring(self, img):
+        # img = cv2.GaussianBlur(img, (5, 5), 0)
+        # img = cv2.bilateralFilter(img, 9, 75, 75)
+        img = cv2.blur(img, (3, 3))
+        return img
+
+
+    # IMAGE HERE
+    def extract_single_image(self, img, RGB=False, plot=False):
+        LBP, hist = self.compute_LBP(img, RGB=RGB, plot=plot)
+        # Return the LBP image and the histogram
+        return LBP, hist
+
+
+    def extract_multiple_images(self, path, RGB=False, plot=False, use_prev_data=False):
+        # Classes of images (0 if not given object, 1 otherwise)
+        classes = []
+        # Features - merged histograms
+        features = np.empty(0)
+
+        # Compute histogram for every file in directory
+        for imagePath in paths.list_images(path):
+            _, hist = self.extract_single_image(self.__read_image__(imagePath), RGB=RGB, plot=plot)
+
+            # Create a list of classes (0 if there is "notpir" in filename)
+            if not use_prev_data:
+                if "notpir" in imagePath:
+                    classes.append(0)
+                else:
+                    classes.append(1)
+
+            # The image features which will be returned
+            if len(features) == 0:
+                features = hist
+            else:
+                features = np.vstack((features, hist))
+
+        # Convert classes into numpy array
+        classes = np.asarray(classes)
+        return features, classes
+
+
+
+
+
+    # Method which divides single image into windows
+    def predict_single_image(self, classifier, imgPath, threshold, rows=11, columns=14, RGB=False, plot=False, plot_rect=False, proba=False):
+        # RGB - prediction for RGB-LBP method;      plot_rect - plot a rectangle on the current window's position to see
+        # if evetything works fine;     rows and columns - (...) on plot;   plot - show a plot;
+        # threshold - value of probability needed for each sample for being classified as "1"
+        # proba - print probability instead of full prediction in plot's title
+
+        image = self.__read_image__(imgPath)
+        # Blurring
+        # image = blurring(image)
+
+        responses = []
+        # Divide image into windows
+        # image_size is (300, 200), but the real one is REVERSED [cv2.resize function]
+        img_size = self.image_size[::-1]
+        percentage = 0.2 # percentage of cropped image relative to its original size
+        counter = 0.2 # is added in every loop iteration
+        i=0
+        while percentage < 1.09:
+            x = 0
+            y = 0
+            cr_img_size = (int(img_size[0]*percentage), int(img_size[1]*percentage))
+            while x < img_size[0] and y < img_size[1]:
+                i = i+1
+                cropped_image = image[x:cr_img_size[0]+x, y:cr_img_size[1]+y]
+                cropped_image = cv2.resize(cropped_image, self.image_size)
+
+                # Plot a rectangle
+                if plot:
+                    if not plot_rect:
+                        plt.subplot(rows,columns,i)
+                        plt.imshow(cropped_image)
+                    else:
+                        img_with_rectangle = cv2.rectangle(image, (y,x), (cr_img_size[1]+y,cr_img_size[0]+x), (255,int((percentage+0.4)*255),int((percentage+0.4)*255)),2)
+                        plt.imshow(img_with_rectangle)
+                        plt.show()
+
+                _, features = self.extract_single_image(cropped_image, RGB=RGB)
+
+                if not proba:
+                    # if we want to see the probability instead of full prediction
+                    responses.append(classifier.predict(features.reshape(1,-1)))
+                    if plot:
+                        if responses[i-1] == 1: # alternatively: if responses[i-1][0][1] >= 0.55:
+                            plt.title("Pyramid")
+                        else:
+                            plt.title("Null")
+                else:
+                    responses.append(classifier.predict_proba(features.reshape(1, -1)))
+                    # round to two decimal places
+                    responses[i - 1] = np.around(responses[i-1], 2)
+                    if plot:
+                        plt.title(str(responses[i - 1]))
+
+                y = int(cr_img_size[1]/2)+y
+                if y >= img_size[1]:
+                    x = int(cr_img_size[0]/2)+x
+                    if x < img_size[0]:
+                        y = 0
+                    # leave current y value for while stop condition otherwise
+            percentage = percentage + counter
+            if percentage >= 0.6:
+                counter = 0.4
+        if plot:
+            plt.show()
+
+        # Check every response
+        res = 0
+        for r in responses:
+            # modify some condition depending on selected type of response
+            if proba:
+                cond = r[0][1] >= threshold
+            else:
+                cond = r == 1
+            if cond:
+                res = 1
+        # if any is equal 1 - there is a pyramid on the image
+        return responses, res
+
+
+    def predict_multiple_images(self, classifier, RGB=False, plot=False, proba=False):
+        features2, _ = self.extract_multiple_images('Images/Test', RGB=RGB, plot=plot, use_prev_data=True)
+        if not proba:
+            predictions = classifier.predict(features2)
+        else:
+            predictions = classifier.predict_proba(features2)
+
+        # plot images
+        i= 1
+        for imagePath in paths.list_images('Images/Test'):
+            plt.subplot(6,5,i)
+            plt.imshow(self.__read_image__(imagePath))
+
+            if not proba:
+                if predictions[i-1] == 0:
+                    plt.title("Null")
+                else:
+                    plt.title("Pyramid")
+            else:
+                plt.title(str(predictions[i-1]))
+            i = i+1
+        plt.show()
+        return predictions
+
+
+
+    def advanced_predict_multiple_images(self, classifier, threshold, RGB=False, plot=False, plot_rect=False):
+        predictions = []
+        i = 1
+        for imagePath in paths.list_images('Images/Test'):
+            _, resp = self.predict_single_image(classifier, imagePath, threshold, RGB=RGB, plot=plot, plot_rect=plot_rect, proba=True)
+            predictions.append(resp)
+            # plot images
+            plt.subplot(6,5,i)
+            plt.imshow(self.__read_image__(imagePath))
+            if predictions[i-1] == 0:
+                plt.title("Null")
+            else:
+                plt.title("Pyramid")
+            i = i+1
+        plt.show()
+        return predictions
+
