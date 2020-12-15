bdbc01669cd0071a37ab0acf5d4dcc2c25bbc8e6
Wojciech7738 <wwbutrym@gmail.com>
2020-12-15 03:51:42 +0100

    optimized lbp

--- fun2.py
+++ fun2.py
@@ -9,8 +9,6 @@ def Main():
     # extract an matrix of histograms and vector of classes
     begin = time.time()
     features, classes = LBP.extract_multiple_images('Images/Train')
-    end = time.time()
-    print(end-begin)
 
     # Construct the strong classifier from image features (week classifier)
     classifier = se.AdaBoostClassifier(n_estimators=features.shape[1], random_state=0)
@@ -24,6 +22,8 @@ def Main():
     _, response = LBP.predict_single_image(classifier, 'Images/Test/walshs-pyramid---south-of-cairn-36649_1280x731.jpg', 0.52, rows=10, columns=15, plot=True, proba=True)
     print(response)
     # advanced_predict_multiple_images(classifier, 0.52) # takes very long time
+    end = time.time()
+    print(end-begin)
     print("DONE")
 
 
