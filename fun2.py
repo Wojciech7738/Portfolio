fb56f355d80d1de0910dcdc2ddc8ff1a75043bfd
Wojciech7738 <wwbutrym@gmail.com>
2021-01-26 21:14:40 +0100

    The RidgeClassifier has been added

--- fun2.py
+++ fun2.py
@@ -1,12 +1,14 @@
 from lbp import LocalBinaryPatterns
 from sklearn.naive_bayes import GaussianNB
 import sklearn.ensemble as se
+import sklearn.linear_model as skl
 import time
 import pickle
 import sys
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from GNaiveBayes_with_Laplace import *
 
 
 def isRGB(RGB):
@@ -31,48 +33,50 @@ def is_etc(string, RGB, norm):
 
 def create_model(object, RGB=False, skip_norm=False):
     # extract an matrix of histograms and vector of classes
-    features, classes = object.extract_multiple_images('Images/Train', RGB=RGB, skip_norm=skip_norm)
-    # Load fetures and classes
-    # filename = "features"
-    # filename = is_etc(filename, RGB, skip_norm)
-    # file = open(filename, 'rb')
-    # features = pickle.load(file)
-    # file.close()
-    # file = open("classes.pkl", 'rb')
-    # classes = pickle.load(file)
-    # file.close()
+    # features, classes = object.extract_multiple_images('Images/Train', RGB=RGB, skip_norm=skip_norm)
+
+    # Load features and classes instead of extraction
+    filename = "features"
+    filename = is_etc(filename, RGB, skip_norm)
+    file = open(filename, 'rb')
+    features = pickle.load(file)
+    file.close()
+    file = open("classes.pkl", 'rb')
+    classes = pickle.load(file)
+    file.close()
 
     # Construct the strong classifier from image features (week classifiers)
     # AdaBoost:
     # model = se.AdaBoostClassifier(n_estimators=128, random_state=0)
     # Naive Bayes
-    model = GaussianNB()
+    # model = GaussianNB()
 
+    # List of models
+    # Winner: RidgeClassifier
+    # MUCH LESS INTERESTED: LinearRegression, Ridge(+CV),
+    model = skl.RidgeClassifier()
     model.fit(features, classes)
 
-    # print(LBP.predict_single_image(model, 'pobrania/egypt-483_960_720.jpeg', 0.59, RGB=RGB, proba=True))
-    _, feat = object.extract_single_image(object.__read_image__('pobrania/egypt-4796260_960_720.jpeg'), RGB=RGB, skip_norm=skip_norm)
-    print(model.predict_proba(feat.reshape(1, -1)))
-
-    # # save model into file
-    filename = "Bclassifier"
+    # save model into file
+    filename = "RCLclassifier"
     filename = is_etc(filename, RGB, skip_norm)
     file = open(filename, 'wb')
     pickle.dump(model, file)
     file.close()
 
-    filename = "features"
-    filename = is_etc(filename, RGB, skip_norm)
-    file = open(filename, 'wb')
-    pickle.dump(features, file)
-    file.close()
+    # filename = "features"
+    # filename = is_etc(filename, RGB, skip_norm)
+    # file = open(filename, 'wb')
+    # pickle.dump(features, file)
+    # file.close()
     # file = open("classes.pkl", 'wb')
     # pickle.dump(classes, file)
     # file.close()
 
 
-def tests(fname, RGB=False, proba=False, debug=False):
-    filename = isRGB(RGB)
+def tests(fname, RGB=False, proba=False, skip_norm=False, debug=False):
+    filename = "RCLclassifier"
+    filename = is_etc(filename, RGB, skip_norm)
 
     # Load model
     file = open(filename, 'rb')
@@ -90,17 +94,21 @@ if __name__ == '__main__':
     lbp_size = (8, 2)
     LBP = LocalBinaryPatterns(*lbp_size)
     RGB = False
+    skip_norm = False
 
     args = sys.argv[1:]
     if args[0] == '1':
         RGB = True
         args.pop(0)
+    if args[0] == 'n':
+        skip_norm = True
+        args.pop(0)
 
     for arg in args:
         if arg in ('--learning', '-l'):
-            create_model(LBP, RGB=RGB, skip_norm=False)
+            create_model(LBP, RGB=RGB, skip_norm=skip_norm)
         elif arg in ('--testing', '-t'):
-            tests(args[0], RGB=RGB, proba=True)
+            tests(args[0], RGB=RGB, skip_norm=skip_norm, proba=False)
 
     print("DONE")
 
